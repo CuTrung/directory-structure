c4094b7fe00beeb3e168d71323ad6d2de6c3748c
CuTrung <cuphanthanhtrung4112003@gmail.com>
2023-06-17 19:18:36 +0700

    create base nest

--- src/api/v1/routes/index.route.js
+++
@@ -1,9 +0,0 @@
-const { checkVersion } = require('@v1/middlewares/index.middleware');
-const routerV1 = require('@v1/routes/v1/index.route');
-module.exports = {
-    initRoutes: (app) => {
-        app.use('/api', checkVersion({
-            '2023-05-19': routerV1
-        }, { defaultVersion: '2023-05-19' }));
-    }
-}
--- src/socket/v1/controllers/index.controller.js
+++
@@ -1,6 +0,0 @@
-const { getStudent } = require('@skv1/services/index.service');
-module.exports = {
-    getStudent: (socket) => {
-        socket.on('getStudent', getStudent(socket))
-    }
-}
